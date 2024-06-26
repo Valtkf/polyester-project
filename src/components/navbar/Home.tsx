abf765b360121cb40cb8914454f624125e2c5e93
Valtkf <valentin.vgregorio@gmail.com>
2024-06-26 17:51:48 +0200

    💄 F02 : Home > Add styling to Home page

--- src/components/navbar/Home.tsx
+++ src/components/navbar/Home.tsx
@@ -3,6 +3,7 @@ import { GiPaintBrush } from "react-icons/gi";
 import { IoMdArrowRoundForward } from "react-icons/io";
 import { RiToolsFill } from "react-icons/ri";
 import { GoMail } from "react-icons/go";
+import { FaPhone } from "react-icons/fa6";
 
 const Home = () => {
   return (
@@ -25,7 +26,7 @@ const Home = () => {
           </div>
         </div>
       </div>
-      <div className="grid grid-cols-1 gap-12 xl:grid-cols-3 mt-[80px] xl:mt-[200px] relative z-20 pr-12 pl-12 font-playfair">
+      <div className="grid grid-cols-1 gap-12 xl:grid-cols-3 mt-[80px] xl:mt-[200px] relative z-20 pr-20 pl-20 font-playfair">
         <div className="container mx-auto">
           <div className="text-center text-xl font-semibold ">
             <div style={{ color: "#f59e0b" }} className="flex justify-center ">
@@ -87,6 +88,52 @@ const Home = () => {
                 <GoMail size={25} />
               </div>
             </a>
+          </div>
+        </div>
+      </div>
+      <div className="mt-[80px] xl:mt-[200px] relative z-20">
+        <div className="container mx-auto xl:px-0 mr-30 ml-30">
+          <div className="flex flex-col xl:flex-row flex-wrap text-center xl:text-left justify-between items-center gap-8 xl:gap-[74px] ">
+            <div className="flex-1 order-2 xl:order-none mt-10 pr-6 pl-6">
+              <h2 className=" text-5xl text-black font-semibold drop-shadow-md font-playfair">
+                Une fabrication sur mesure grâce à notre expertise de la matière
+                polyester
+              </h2>
+              <p className="flex flex-wrap mt-14 pr-4 pl-4 text-xl text-black font-normal">
+                Les domaines sont très variés , ce qui nous permet de répondre à
+                toutes les demandes , du domaine design , de la décoration
+                d'intérieur ou extérieur tout est possible!
+              </p>
+              <div className="flex justify-center items-center mt-10">
+                <div
+                  style={{ color: "#f59e0b" }}
+                  className="flex justify-center items-center bg-orange-100 rounded-full w-[73px] h-[73px] mt-10"
+                >
+                  <FaPhone size={25} />
+                </div>
+                <div className="text-center mt-10 ml-4">
+                  <div className="font-normal text-lg text-gray-700">
+                    0670504090
+                  </div>
+                  <div className="font-normal text-lg text-gray-700">
+                    Appelez nous
+                  </div>
+                </div>
+              </div>
+              <button
+                type="button"
+                className="mt-20 text-gray-900 bg-white border border-gray-300 focus:outline-none hover:bg-gray-100 focus:ring-4 focus:ring-gray-100 font-medium rounded-lg text-lg px-5 py-2.5 me-2 mb-2 dark:bg-gray-800 dark:text-white dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-600 dark:focus:ring-gray-700"
+              >
+                Envoyer un mail
+              </button>
+            </div>
+            <div className="max-w-[453px] order-1 xl:order-none">
+              <img
+                src="../../../images/swimmingpool.jpg"
+                alt=""
+                className="rounded-tr-[200px] rounded-bl-[100px] rounded-br-2xl rounded-tl-2xl"
+              />
+            </div>
           </div>
         </div>
       </div>
